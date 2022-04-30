8b87e86979f68bf74e8198c72af2ef47684537bd
Annie Huang <annie.hn@gmail.com>
2022-04-30 17:12:58 +1000

    Add register into each input within the form and you can see that we can get the value from the handleSubmit function

--- src/components/Basic.tsx
+++ src/components/Basic.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 let renderCount = 0;
 const Basic = () => {
+  const { register, handleSubmit } = useForm();
   renderCount++;
+
   return (
     <div>
       Render Count: {renderCount}
+      {/*
       <form>
         <input name='firstName' placeholder='First Name' />
         <input name='lastName' placeholder='Last Name' />
+        <input type='submit' />
+      </form>
+      */}
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input {...register('firstName')} placeholder='First Name' />
+        <input {...register('lastName')} placeholder='Last Name' />
         <input type='submit' />
       </form>
     </div>
